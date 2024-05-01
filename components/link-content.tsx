9cca0a447fd7ed6677c60dd568c2eb6640142e22
chuhemiao <535101602@qq.com>
2024-05-01 15:46:54 +0800

    init

--- components/link-content.tsx
+++ components/link-content.tsx
@@ -60,40 +60,17 @@ export function LinkContent() {
         <div className="mb-12">
           <div className="my-4">
             <h1 className="mb-2 text-2xl font-bold text-primary/80 sm:text-3xl">
-              联系我
+              更多加密货币内容，请通过视频学习！！！
             </h1>
           </div>
           <div>
-            你可以通过
-            <Link
-              href="mailto:2020583117@qq.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <span className="px-3 underline decoration-wavy hover:text-purple-500">
-                Email
-              </span>
-            </Link>
-            或者
-            <Link
-              href="https://weixin.sogou.com/weixin?type=1&query=kk%E6%83%B3%E5%BD%93%E7%A8%8B%E5%BA%8F%E5%91%98"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <span className="px-3 underline decoration-wavy hover:text-purple-500">
-                微信公众号
-              </span>
-            </Link>
-            联系我
-          </div>
-          <div>
-            <Image
-              src="/weixin.jpg"
-              className="float-left md:w-1/4"
-              alt="kk想当程序员"
-              width={318}
-              height={318}
-            />
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/CROkruTahjY?si=4pfM-f2QaVpSNvYV"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            ></iframe>
           </div>
         </div>
       </div>
